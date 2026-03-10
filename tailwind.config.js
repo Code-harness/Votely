afd0b904a614968e65612fd61f3920d448edd9dc
Code-harness <codeharness1@gmail.com>
2026-03-10 21:08:43 +0200

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./app/**/*.php",
+        "./routes/**/*.php",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
